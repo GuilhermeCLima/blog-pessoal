962f25055d6da0387ddc75c0c975b0a83e3830ab
guilherme lima <guilehrme.cl.lima@gmail.com>
2020-09-02 19:52:26 -0300

    adicionar post blogpessoal

--- blogPessoal/src/app/feed/feed.component.ts
+++ blogPessoal/src/app/feed/feed.component.ts
@@ -13,6 +13,9 @@ import { TemaService } from "../service/tema.service"
 })
 export class FeedComponent implements OnInit {
 
+  key: 'data'
+  reverse
+
   postagem: Postagem = new Postagem()
   listaPostagens: Postagem[]
 
@@ -35,6 +38,21 @@ export class FeedComponent implements OnInit {
 
   findallPostagens() {
     this.postagemService.getAllPostagens().subscribe((resp: Postagem[]) => this.listaPostagens = resp)
+  }
+  publicar(){
+    this.tema.id = this.idTema
+    this.postagem.tema = this.tema
+
+    if(this.postagem.titulo == null || this.postagem.texto == null || this.postagem.tema == null){
+      alert('Preencha todos os campos antes de publicar!')
+    }else{
+      this.postagemService.postPostagem(this.postagem).subscribe((resp: Postagem)=>{
+        this.postagem = resp
+        this.postagem = new Postagem()
+        alert('Postagem realizada com sucesso!')
+        this.findallPostagens()
+      })
+    }
   }
   findAllTemas() {
     this.temaService.getAllTemas().subscribe((resp: Tema[]) => { this.listaTemas = resp })
--- blogPessoal/src/app/service/postagem.service.ts
+++ blogPessoal/src/app/service/postagem.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Postagem } from '../model/Postagem';
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +15,8 @@ export class PostagemService {
 
   getAllPostagens(){
     return this.http.get('http://localhost:9000/postagens',this.token)
+  }
+  postPostagem(postagem : Postagem){
+    return this.http.post('http://localhost:9000/postagens',postagem,this.token)
   }
 }
